f259587934b2c72273fde818f34aa070360ea9ac
Chronomanteca <danisan98@hotmail.es>
2025-05-22 22:10:17 -0500

    Finished exercise 11 temperature conversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  convertedTemp = (temp-32)*5/9;
+  return convertedTemp == 0 ? convertedTemp: parseFloat(convertedTemp.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  convertedTemp = (temp*9/5)+32;
+  return convertedTemp == 0 ? convertedTemp: parseFloat(convertedTemp.toFixed(1));
 };
 
 // Do not edit below this line
