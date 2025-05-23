af69aa5c9dfbfd0ce7987ddcb04412cc6423c96b
Chronomanteca <danisan98@hotmail.es>
2025-05-22 20:42:21 -0500

    Finished exercise 8 Remove from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
 const removeFromArray = function() {
+
+    let array = arguments[0];
+    
+    for(i=1;i<arguments.length;i++){
+        let elementRemove = arguments[i];
+        for(j=0; j<array.length;j++){
+
+            let indexItemRemove = array.findIndex((item) => (item == elementRemove && typeof item) == typeof elementRemove);
+            if(indexItemRemove > -1){
+                array.splice(indexItemRemove,1);
+            }
+            
+        }
+        
+    }    
+    
+    return array;
+
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
