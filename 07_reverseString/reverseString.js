ec9ad0f1599f429375a609bbd2cb931c2241ac44
Chronomanteca <danisan98@hotmail.es>
2025-05-22 20:19:02 -0500

    Finished exercise 7 reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    reversedPhrase = "";
+    for(i = 0; i<phrase.length; i++){
+        reversedPhrase = reversedPhrase +phrase[(phrase.length-1)-i];
+    }
 
+    return  reversedPhrase;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
