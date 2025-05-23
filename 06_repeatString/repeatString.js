6040f03aa73ee99bb20f1b619f8bff80578dd407
Chronomanteca <danisan98@hotmail.es>
2025-05-22 19:15:12 -0500

    Finished exercise 6 repeat String

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(phrase, repeatCount) {
+    if(repeatCount>=0){
+        repeatedPhrase = "";
+        for(i = 0;i<repeatCount; i++){
+            repeatedPhrase =repeatedPhrase + phrase;
+        }  
+        return repeatedPhrase;
+
+    }
+    return "ERROR";
 
 };
 
