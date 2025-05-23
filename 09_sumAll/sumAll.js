2cf25c4f497ae09bcd81018bdc821824171fd37c
Chronomanteca <danisan98@hotmail.es>
2025-05-22 20:51:37 -0500

    Finished exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+    if(Number.isInteger(a) == Number.isInteger(b) == true){
+        if(a >= 0 && b >=0 && a){
+            sum = 0;
+            if(a>b){
+                [a,b] = [b,a];
+            }
+            for(i=a;i<=b;i++){
+                sum = sum+i;
+            }
+            return sum;
+        }
+    }
+
+    return "ERROR";
+
 
 };
 
